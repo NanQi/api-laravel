1ae542fc412bcffb71abaf6f7e3c513e1c359886
nanqi <luyu_1216@126.com>
2019-09-13 15:43:43 +0800

    default length;

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use App\Exceptions\ApiExceptionHandler;
 use Config;
@@ -16,6 +17,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Schema::defaultStringLength(200);
     }
 
     /**
